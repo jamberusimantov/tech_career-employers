aa75259582c524a4aa21c79e59d80ae13fbdd0ca
jamberusimantov <73583675+jamberusimantov@users.noreply.github.com>
2021-05-18 18:27:52 +0300

    removed footer comments

--- client/src/components/Layout/Footer.tsx
+++ client/src/components/Layout/Footer.tsx
@@ -1,10 +1,11 @@
 import React from 'react'
 
 const Footer = () => {
-    // return (
-    //     <footer>
-    //         <h1>respect: Team2Rules@TechCareer2020</h1>
-    //     </footer>
-    // )
+    return (
+         <footer>
+             <h1>respect: Team2Rules@TechCareer2020</h1>
+    
+         </footer>
+     )
 }
 export default Footer;
