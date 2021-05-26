c663125bc7e52495fc2f3c5266a072d61d65b56b
tal556 <tall5225@gmail.com>
2021-05-26 13:57:06 +0300

    changes in layout

--- client/src/components/Layout1/LayoutMain.tsx
+++ client/src/components/Layout1/LayoutMain.tsx
@@ -17,8 +17,8 @@ function LayoutMain() {
   return (
     <Layout>
       <Header className="header">
-        <div className="logo" />
-        <Menu theme="dark" mode="horizontal" defaultSelectedKeys={["2"]}>
+        <img src="/img/Logo.png" alt="" className="logo"/>
+        <Menu theme="light" mode="horizontal" defaultSelectedKeys={["2"]}>
           <Menu.Item key="1">nav 1</Menu.Item>
           <Menu.Item key="2">nav 2</Menu.Item>
           <Menu.Item key="3">nav 3</Menu.Item>
