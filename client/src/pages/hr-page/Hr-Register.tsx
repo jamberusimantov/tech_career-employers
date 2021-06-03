5168a27ed3255fd55668bf2ecbde7e17551e4e87
OR_GOLESTANI <orgolestani@gmail.com>
2021-06-03 16:06:00 +0300

    hr-reg-form : usestate bug fix

--- client/src/pages/hr-page/Hr-Register.tsx
+++ client/src/pages/hr-page/Hr-Register.tsx
@@ -1,4 +1,5 @@
-import React, { useState } from "react";
+import * as React from 'react';
+import {useState}  from 'react';
 import ReactDOM from "react-dom";
 import "antd/dist/antd.css";
 import {
