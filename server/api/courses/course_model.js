81b56e974dde30007181bf99e84f38be5e11763a
liori142 <liorimazgi@gmail.com>
2021-06-04 19:39:28 +0300

    course model and router fix

--- server/api/courses/course_model.js
+++ server/api/courses/course_model.js
@@ -4,6 +4,8 @@ const Schema = mongoose.Schema;
 const course = new Schema({
     courseName: {type:String ,required:true},
     courseCompletionDate: {type:String ,required:true},
+    numberOfStudents: {type:Number ,required:true},
+    idsOfGraduates:{type:Array ,required:true},
     numberOfGraduates: {type:Number ,required:true},
     graduatesWorking:{type:Number ,required:true},
     graduatesNotWorking:{type:Number ,required:true},
--- server/api/courses/course_router.js
+++ server/api/courses/course_router.js
@@ -3,8 +3,8 @@ const course_ctrl = require('./course_ctrl')
 
 courseRouter.get('/all', course_ctrl.getAllCourses)
 courseRouter.post('/', course_ctrl.createCourse)
-courseRouter.put('/:id', course_ctrl.updateCourseByUrlId)
-courseRouter.delete('/:id', course_ctrl.deleteCourseByUrlId)
+courseRouter.put('/:Id', course_ctrl.updateCourseByUrlId)
+courseRouter.delete('/:Id', course_ctrl.deleteCourseByUrlId)
 
 module.exports = courseRouter;
 
