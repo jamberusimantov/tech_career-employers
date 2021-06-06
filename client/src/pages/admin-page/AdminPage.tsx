e56f959dff1b56db8221505306af03923b0a9a9e
mosmo46 <73312834+mosmo46@users.noreply.github.com>
2021-06-06 19:25:15 +0300

    add search to table

--- client/src/pages/admin-page/AdminPage.tsx
+++ client/src/pages/admin-page/AdminPage.tsx
@@ -7,27 +7,46 @@ import CodeinTable from "../../components/shared/CodeinTable";
 
 import service from '../../utils';
 
+import './AdminPage.css'
 
 import {getAllCourses } from './admin.service'
 
 
 
-const columns = [
+const coursesColumns = [
   {
     title: 'שם הקורס',
     dataIndex: 'courseName',
     key: 'courseName',
-  },
+    filters: [
+      {
+        text: 'Full-Stack',
+        value: 'Full-Stack',
+      },
+      {
+        text: 'QA',
+        value: 'QA',
+      },
+      {
+        text: 'Cyber',
+        value: 'Cyber',
+      },
+    ],
+    onFilter: (value: any, record: { courseName: string | any[]; }) => record.courseName.indexOf(value) === 0,
+    // sorter: (a: { name: string | any[]; }, b: { name: string | any[]; }) => a.name.length - b.name.length,
+    sortDirections: ['descend'],  },
   {
     title: 'מועד הסיום',
     dataIndex: 'courseCompletionDate',
     key: 'courseCompletionDate',
+    // sorter:  sortByName,
+		// onFilter: filterByName,
   },
   {
     title: 'מס בוגרים',
     dataIndex: 'numberOfGraduates',
     key: 'numberOfGraduates',
-    
+
   },
   {
     title: 'מס מועסקים',
@@ -47,7 +66,48 @@ const columns = [
 ];
 
 
+
+export const graduatesColumns: any[] = [
+  {
+    title: 'חברה',
+    dataIndex: 'company',
+  },
+  {
+    title: 'מגייסת',
+    dataIndex: 'uploadedBy',
+  },
+  {
+    title: 'תפקיד',
+    dataIndex: 'passion',
+  },
+  {
+    title: 'ת. פתיחת משרה',
+    dataIndex: 'uploadDate',
+  },
+  {
+    title: 'סטטוס',
+    dataIndex: 'age',
+  },
+  {
+    title: 'שאל את המגייסת?',
+    dataIndex: 'emailTo',
+    render: (text: string) =>
+      // eslint-disable-next-line jsx-a11y/anchor-is-valid
+      <a href="mailto: abc@example.com">{text}</a>,
+  },
+  {
+    title: 'הגישו קו"ח',
+    dataIndex: 'name',
+    // render: (text: string) => <a>{text}</a>,
+  },
+];
+
+
 function AdminPage() {
+
+
+  const [searchValueInTable, setSearchValueInTable] = useState('');
+
 
   const { login } = service
 
@@ -87,9 +147,6 @@ function AdminPage() {
     setIsModalVisibleHr(true);
   };
   
-  const handleOkHr = () => {
-    setIsModalVisibleHr(false);
-  };
   
   const handleCancelHr = () => {
     setIsModalVisibleHr(false);
@@ -110,10 +167,29 @@ function AdminPage() {
     console.log(registerHr.credentials);
   }
 
+  function sortByName(a: { courseCompletionDate: { toLowerCase: () => number; }; }, b: { courseCompletionDate: { toLowerCase: () => number; }; }) {
+    return a.courseCompletionDate.toLowerCase() - b.courseCompletionDate.toLowerCase();
+  }
+  
+  // function filterByName(value: any, record: { courseCompletionDate: string | any[]; }) {
+  //   return record.courseCompletionDate.indexOf(value) !== -1;
+  // }
+
 
   return (
     <div className="admin-page">
+      <Input.Search
+       placeholder="חפש"
+       value={searchValueInTable}
+       />
+
+
+
       <div className="modal-admin-page">
+
+
+
+
         <Button type="primary" onClick={showModalStudent}>
           רישום סטודנט
         </Button>
@@ -156,13 +232,13 @@ function AdminPage() {
         {!showCoursesTable ? (
           " "
         ) : (
-          <CodeinTable  columns={columns} getData={getAllCourses} />
+          <CodeinTable columns={coursesColumns} getData={getAllCourses} />
         )}
-        {/* {!showGraduatesTable ? (
+        {!showGraduatesTable ? (
           " "
         ) : (
-          <CodeinTable columns={graduatesColumns} data={graduatesData} />
-        )} */}
+          <CodeinTable columns={graduatesColumns}  />
+        )}
       </div>
     </div>
   );
