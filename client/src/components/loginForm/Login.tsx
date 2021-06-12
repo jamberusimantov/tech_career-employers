bb257447dae47fa56f31b8a1512d815be6d6f3ac
tal556 <tall5225@gmail.com>
2021-06-12 17:21:13 +0300

    fix logIn file

--- client/src/components/loginForm/Login.tsx
+++ client/src/components/loginForm/Login.tsx
@@ -1,21 +1,15 @@
-import React, { useState } from "react";
-import { Form, Input, Button, Select } from "antd";
-import { connect } from "react-redux";
-import usersActions from "../../redux/actions/user.actions";
-import LogIn from "../../utils/login.utils";
+import React,{useState} from 'react'
+import { Form, Input, Button, Select } from 'antd';
+import { connect } from 'react-redux';
+import usersActions from '../../redux/actions/user.actions';
+import LogIn from '../../utils/login.utils';
 const { Option } = Select;
 
-const { loginUser, setTokenLocal } = LogIn;
+const { loginUser, setTokenLocal } = LogIn
 const { setUserData } = usersActions.usersActions;
 
-const mapDispatchToProps = (dispatch: any) => ({
-  setUserData: (data: Object) => {
-    dispatch(setUserData(data));
-  },
-});
-const mapStateToProps = (state: any) => {
-  return { userData: state.user.userData };
-};
+const mapDispatchToProps = (dispatch: any) => ({setUserData: (data: Object) => { dispatch(setUserData(data)) }})
+const mapStateToProps = (state: any) => { return { userData: state.user.userData } }
 
 const Login = (): any => {
     const [role, setRole] = useState('student')
@@ -38,68 +32,50 @@ const Login = (): any => {
 
         } else { alert('נסה שוב') }
 
-  // const onFinish = async (values: any) => {
-  //   const email: any = values.email;
-  //   const password: any = values.password;
-  //   const resFromLogin = await loginUser({ email, password }, role);
-  //   if (resFromLogin.success) {
-  //     const token = resFromLogin.token;
-  //     setTokenLocal(token);
-  //     setUserData(token);
-  //     window.location.reload();
-  //   } else {
-  //     alert("failed!");
-  //   }
-  // };
-  const onFinishFailed = (errorInfo: any) => {
-    console.log("Failed:", errorInfo);
-  };
-  const handleChange = (value: any) => {
-    setRole(value);
-  };
-  return (
-    <Form
-      {...layout}
-      name="basic"
-      initialValues={{ remember: true }}
-      onFinish={onFinish}
-      onFinishFailed={onFinishFailed}
-    >
-      <Form.Item
-        label="אימייל"
-        name="email"
-        rules={[{ required: true, message: "הכנס אימייל!" }]}
-      >
-        <Input />
-      </Form.Item>
-
-      <Form.Item
-        label="סיסמא"
-        name="password"
-        rules={[{ required: true, message: "הכנס סיסמא!" }]}
-      >
-        <Input.Password />
-      </Form.Item>
-
-      <div style={{ display: "flex", justifyContent: "space-between" }}>
-        <Form.Item {...tailLayout}>
-          <Button type="primary" htmlType="submit">
-            {" "}
-            התחבר{" "}
-          </Button>
-        </Form.Item>
-        <Select
-          defaultValue="student"
-          style={{ width: 120, height: "110%" }}
-          onChange={handleChange}
+    }
+    const onFinishFailed = (errorInfo: any) => { console.log('Failed:', errorInfo) }
+    const handleChange = (value: any) =>{
+        setRole(value)
+    }
+    return (
+        <Form
+            {...layout}
+            name="basic"
+            initialValues={{ remember: true }}
+            onFinish={onFinish}
+            onFinishFailed={onFinishFailed}
         >
-          <Option value="student">Student</Option>
-          <Option value="hr">Hr</Option>
-          <Option value="admin">Admin</Option>
-        </Select>
-      </div>
-    </Form>
-  );
+            <Form.Item
+                label="אימייל"
+                name="email"
+                rules={[{ required: true, message: 'הכנס אימייל!' }]}
+            >
+                <Input />
+            </Form.Item>
+
+            <Form.Item
+                label="סיסמא"
+                name="password"
+                rules={[{ required: true, message: 'הכנס סיסמא!' }]}
+            >
+                <Input.Password />
+            </Form.Item>
+
+
+            <div style={{ display: 'flex', justifyContent: 'space-between', }}>
+                <Form.Item  {...tailLayout} >
+                    <Button type="primary" htmlType="submit"> התחבר </Button>
+                </Form.Item>
+                <Select defaultValue="student" style={{ width: 120, height: '110%' }} onChange={handleChange}>
+                    <Option value="student">Student</Option>
+                    <Option value="hr">Hr</Option>
+                    <Option value="admin">Admin</Option>
+                </Select>
+            </div>
+        </Form>
+    );
 };
-}
-export default connect(mapStateToProps, mapDispatchToProps)(Login);
+
+
+
+export default connect(mapStateToProps, mapDispatchToProps)(Login)
