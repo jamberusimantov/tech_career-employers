8d357aac66e1c0dfdbb2771376576dde39a494b5
rbenenosh <rbenenosh@ebay.com>
2021-06-13 13:29:06 +0300

    Add more stories

--- client/src/components/slider/Slider.tsx
+++ client/src/components/slider/Slider.tsx
@@ -1,17 +1,17 @@
 import React from "react";
 import { Carousel } from "antd";
-import "./Slider.css";
+import "./index.css";
 
 export default function Slider() {
   return (
     <div className="sliderContainer">
       <div className="sliderSmallContainer">
         <Carousel autoplay>
-         <img src="./images/11.jpg" alt="N/A" />
-         <img src="./images/22.jpeg" alt="N/A" />
-         <img src="./images/33.jpeg" alt="N/A" />
-         <img src="./images/44.jpeg" alt="N/A" />
-         <img src="./images/55.jpeg" alt="N/A" />
+          <img src="./images/11.jpg" alt="N/A" />
+          <img src="./images/22.jpeg" alt="N/A" />
+          <img src="./images/33.jpeg" alt="N/A" />
+          <img src="./images/44.jpeg" alt="N/A" />
+          <img src="./images/55.jpeg" alt="N/A" />
         </Carousel>
       </div>
     </div>
